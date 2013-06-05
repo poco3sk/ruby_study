9fd2ad4322697ab4c4557bd100d5aff043f4cc42
krishna <skyblue_flare@yahoo.co.jp>
2013-06-05 12:07:10 +0900

    modified. p.140 refactoring

--- refactoring/lib/chap_6/replace_temp_with_query_sample.rb
+++ refactoring/lib/chap_6/replace_temp_with_query_sample.rb
@@ -18,14 +18,20 @@ class ReplaceTempWithQuerySample
   #
   # @return [Fixnum]
   def price
-    base_price = @quantity * @item_price
-
-    if base_price > 1000
-      discount_factor = 0.95
-    else
-      discount_factor = 0.98
-    end
-
     base_price * discount_factor
+  end
+
+  # base price
+  #
+  # @return [Integer]
+  def base_price
+    @quantity * @item_price
+  end
+
+  # discount factor
+  #
+  # @return [Fixnum]
+  def discount_factor
+    base_price > 1000 ? 0.95 : 0.98
   end
 end
