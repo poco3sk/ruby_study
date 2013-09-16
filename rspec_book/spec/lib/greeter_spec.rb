0da2917a45f0dcc9557fba1a6d8921ad0676216c
krishna <skyblue_flare@yahoo.co.jp>
2013-09-16 16:24:36 +0900

    new. RSpec Book p.13

---
+++ rspec_book/spec/lib/greeter_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "RSpec Greeter" do
+  it "should say 'Hello RSpec!' when it receives the greet() message" do
+    greeter = RSpecGreeter.new
+    greeting = greeter.greet
+    expect(greeting).to eq("Hello RSpec!")
+  end
+end
+
